5152a217a6d26390a706fe1eca5060e6aa601d04
diego2608 <diegoif26@gmail.com>
2017-02-06 16:02:27 -0500

    Initial Commit

---
+++ clase5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clase5
+//
+//  Created by Diego on 6/02/17.
+//  Copyright © 2017 Consorcio HBO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
